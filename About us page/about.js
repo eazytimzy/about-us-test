bd11ca4326ac79d4951b5a19d9ce86c5abf21868
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-05-28 14:53:02 +0100

    chore:responsivness fixed

--- About us page/about.js
+++ About us page/about.js
@@ -6,3 +6,29 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+// function to display contact of team members
+function eazycon() {
+document.getElementById("eazy");
+document.getElementById("tim");
+tim.style.display = "block"
+
+}
+
+function sophie() {
+  document.getElementById("dam1");
+  document.getElementById("dam2");
+  dam2.style.display = "block"
+  }
+
+  function ini() {
+    document.getElementById("fam1");
+    document.getElementById("fam2");
+    fam2.style.display = "block"
+    }
+
+    function authur() {
+      document.getElementById("sam1");
+      document.getElementById("sam2");
+      sam2.style.display = "block"
+      }
